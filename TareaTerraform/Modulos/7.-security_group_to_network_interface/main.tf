e95522cb2cf26faabf7d01d19566630bc199e8eb
mauro alvarez <mauro-manuel.alvarez-suarez.external@capgemini.com>
2023-05-04 13:04:05 +0200

    Cambios en main.tf de modulo 7,var.

--- TareaTerraform/Modulos/7.-security_group_to_network_interface/main.tf
+++ TareaTerraform/Modulos/7.-security_group_to_network_interface/main.tf
@@ -1,5 +1,5 @@
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = var.network_interface_id
-  network_security_group_id = var.network_security_group_id
+  network_interface_id      = var_network_interface.id
+  network_security_group_id = var_network_security_group.id
 }
