4b84bf82c5061eae08532655be1c056d05ef00b8
Juan <juan.rodriguez-garcia-san-pedro.external@capgemini.com>
2023-05-04 12:24:59 +0200

    Arreglo modulo 2 (name)

--- TareaTerraform/Modulos/2.-virtual_network/main.tf
+++ TareaTerraform/Modulos/2.-virtual_network/main.tf
@@ -1,6 +1,6 @@
 # Create virtual network
 resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "${random_pet.prefix.id}-vnet"
+  name                = var.name
   address_space       = var.address_space
   location            = var.location
   resource_group_name = var.resource_group_name
