e3ad88b808a5b767a91680e4366ac7cf9a02ed6e
Adrian Naves <adrian.naves-de-la-fuente.external@capgemini.com>
2023-05-04 09:26:49 +0200

    Creamos toda la estructura de los modulos

---
+++ TareaTerraform/Modulos/security_group_to_network_interface/main.tf
@@ -0,0 +1,5 @@
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
