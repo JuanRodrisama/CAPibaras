2ed376a78c9ccbafdac8259d1a72f3a9aea478f4
Lafuente Migueles external <miguel-angel.lafuente-migueles.external@capgemini.com>
2023-05-04 13:15:00 +0200

    Modulo 5, creación del archivo variable.tfvars y adaptación de los archivos .tf

--- TareaTerraform/Modulos/5.-network_security_group/main.tf
+++ TareaTerraform/Modulos/5.-network_security_group/main.tf
@@ -1,29 +1,29 @@
 # Create Network Security Group and rules
 resource "azurerm_network_security_group" "my_terraform_nsg" {
-  name                = "${random_pet.prefix.id}-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name_network_security       = var.name_network_security
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
 
   security_rule {
-    name                       = "RDP"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+    name_ssh                       = var.name_ssh
+    priority_ssh                   = var.priority_ssh
+    direction_ssh                  = var.direction_ssh
+    access_ssh                     = var.access_ssh
+    protocol_ssh                   = var.protocol_ssh
+    source_port_range_ssh          = var.source_port_range_ssh
+    destination_port_range_ssh     = var.destination_port_range_ssh
+    source_address_prefix_ssh      = var.source_address_prefix_ssh
+    destination_address_prefix_ssh = var.destination_address_prefix_ssh
   }
   security_rule {
-    name                       = "web"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+    name_http                       = var.name_http
+    priority_http                   = var.priority_http
+    direction_http                  = var.direction_http
+    access_http                     = var.access_http
+    protocol_http                   = var.protocol_http
+    source_port_range_http          = var.source_port_range_http
+    destination_port_range_http     = var.destination_port_range_http 
+    source_address_prefix_http      = var.source_address_prefix_http 
+    destination_address_prefix_http = var.destination_address_prefix_http
   }
 }
