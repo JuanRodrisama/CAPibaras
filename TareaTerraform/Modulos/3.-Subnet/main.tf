aa25987456a27cad63fdb350bfea7928f09c8685
Juan <juan.rodriguez-garcia-san-pedro.external@capgemini.com>
2023-05-04 13:09:07 +0200

    Corrección módulo 3

--- TareaTerraform/Modulos/3.-Subnet/main.tf
+++ TareaTerraform/Modulos/3.-Subnet/main.tf
@@ -1,7 +1,6 @@
 resource "azurerm_subnet" "my_terraform_subnet" {
-locals {
-  subnet_name = "${random_pet.prefix.id}-subnet"
-}
+  
+  name_subnet = "${var.name}-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.subnet_address_prefixes
