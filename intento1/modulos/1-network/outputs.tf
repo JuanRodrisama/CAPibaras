cf80b74a5d7da8253b121010b980458f2b36f2a5
JuanRodrisama <131357210+JuanRodrisama@users.noreply.github.com>
2023-06-02 09:56:08 +0200

    Delete intento1 directory

--- intento1/modulos/1-network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subnet_id" {
-    value = azurerm_subnet.subnet.id
-}
-output "name_rg" {
-    value = azurerm_virtual_network.vnet.resource_group_name
-}
-output "location" {
-    value = azurerm_virtual_network.vnet.location
-}
