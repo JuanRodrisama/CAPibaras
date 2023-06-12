77a26f57b6d5691b26a474ad71bfd4ee2674d7ac
JuanRodrisama <131357210+JuanRodrisama@users.noreply.github.com>
2023-06-12 08:38:17 +0200

    Delete intento3 directory

--- intento3/modulos/1-network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "subnet_web_id" {
-  value = azurerm_subnet.web_subnet.id
-}
-output "subnet_db_id" {
-  value = azurerm_subnet.db_subnet.id
-}
-output "interface_id" {
-  value = azurerm_network_interface.my_nic[*].id
-}
--- intento3/modulos/3-storage/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "os_disk_ids" {
-  value = azurerm_managed_disk.os_disk[*].id
-}
-output "os_disk_names" {
-  value = azurerm_managed_disk.os_disk[*].name
-}
-output "data_disk_ids" {
-  value = azurerm_managed_disk.data_disk[*].id
-}
